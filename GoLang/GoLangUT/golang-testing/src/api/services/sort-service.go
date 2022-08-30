5021d83793e5ff50086c880b01ee657e09b053e5
Sardar <mohammad.sardar@zeiss.com>
2022-08-31 05:27:11 +0530

    init commit

---
+++ GoLang/GoLangUT/golang-testing/src/api/services/sort-service.go
@@ -0,0 +1,8 @@
+package services
+import (
+	"example.com/GoLangUT/golang-testing/src/api/services/utils/sort"
+)
+
+func Sort(elements []int){
+	BubbleSort(elements)
+}
---
+++ GoLang/GoLangUT/golang-testing/src/api/utils/sort_test.go
@@ -0,0 +1,16 @@
+package ut
+
+import "testing"
+
+func TestBubbleSort(t *testing.T) {
+	elements := []int{9, 8, 6, 5, 2}
+
+	BubbleSort(elements)
+
+	if elements[0] != 2 {
+		t.Error("First element should not be 9")
+	}
+	if elements[len(elements)-1] != 9 {
+		t.Error(("last element should not be 2"))
+	}
+}
